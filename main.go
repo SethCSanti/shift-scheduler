7624190ebc675a05e7466e388e5c5429c819868b
Seth Conner <sconner1@byu.edu>
2026-05-28 12:57:09 -0600

    made models.go, added other page routes in main.go

--- main.go
+++ main.go
@@ -5,17 +5,33 @@ import (
     "net/http"
 )
 
-// Define a home handler function which writes a byte slice containing
-// "Hello from Shawn Mendix" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello from Shawn Mendix"))
+}
+    
+func scheduleView(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Schedule view"))
+}
+
+func approvalView(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Approval view"))
+}
+
+func submitView(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Submit view"))
+}
+
+func decideView(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Decide view"))
 }
 
 func main() {
     mux := http.NewServeMux()
-    mux.HandleFunc("GET /{$}", home) // Restrict this route to exact matches on / only.
-    mux.HandleFunc("GET /example", home) // This will match /example and /example/ but not /example/something.
-    mux.HandleFunc("POST /example", home) // This will only match POST requests to /example.
+    mux.HandleFunc("GET /{$}", home) // Returns full page, home page
+    mux.HandleFunc("GET /schedule", scheduleView) // Returns full page, schedule view
+    mux.HandleFunc("GET /approval", approvalView) // Returns full page, approval view
+    mux.HandleFunc("POST /schedule/submit", submitView) // Returns HTML fragment (HTMX), submission view
+    mux.HandleFunc("POST /schedule/decide", decideView) // Returns HTML fragment (HTMX), decision view
 
     log.Print("starting server on https://localhost:4000")
 
